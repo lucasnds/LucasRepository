2db1defe6fda43cbe24a74134910409881db7345
Lucas Nascimento <lucasdoismile16@gmail.com>
2022-04-28 22:41:55 -0300

    Código Fibonacci

---
+++ PackageJava/Exercicio.java
@@ -0,0 +1,24 @@
+package PackageJava;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+
+public static void main(String[] args) {
+int a, b, c, n, i;
+Scanner scan = new Scanner(System.in);
+System.out.println("informe o valor de n:");
+n = scan.nextInt();
+scan.close();
+a=1;
+b=1;
+c=a+b;
+for(i=1; i<=n; i++) {
+System.out.println(a);
+a=b;
+b=c;
+c=a+b;
+}
+}
+}
